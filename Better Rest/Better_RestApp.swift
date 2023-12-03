53463c4403249a5ddd0e38414e6bda43c77c09fc
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-03 20:00:53 +0300

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Omer Cagri Sayir on 3.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
